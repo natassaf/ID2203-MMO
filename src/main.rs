fd1e22f3ecc899b6f61d0b170a3f8a5854a9b73f
Natassa Farmaki <natazf@gmail.com>
2024-03-02 16:55:41 +0100

    fix errors in main

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,17 @@
 pub mod datastore;
 pub mod durability; 
-use durability::{OmniLogEntry, OmniPaxosDurability};
+use durability::{omnipaxos_durability, OmniLogEntry, OmniPaxosDurability};
 pub use omnipaxos_durability::*;
 pub use std::sync::mpsc::channel;
 pub mod node;
-use crate::kv::KVCommand;
-use crate::server::Server;
-use datastore::example_datastore;
 use node::{Node, NodeRunner};
 use omnipaxos::{messages::Message, util::NodeId, *};
 use omnipaxos_storage::memory_storage::MemoryStorage;
-use core::arch;
-use std::{collections::HashMap, env, sync::{Arc, Mutex}, thread::spawn};
-use tokio::{self, runtime::{self, Builder, Runtime}, sync::mpsc};
+use std::{collections::HashMap, sync::{Arc, Mutex}};
+use tokio::{self, runtime::{ Builder, Runtime }, sync::mpsc};
 const SERVERS: [NodeId; 2] = [1, 2];
 
-type OmniPaxosKV = OmniPaxos<KVCommand, MemoryStorage<KVCommand>>;
+type OmniPaxosKV = OmniPaxos<OmniLogEntry, MemoryStorage<OmniLogEntry>>;
 
 fn main() {
     let server_config = ServerConfig {
@@ -35,7 +31,7 @@ fn main() {
     };
     let op_config = OmniPaxosConfig {
         server_config,
-        cluster_config,
+        cluster_config: cluster_config.clone(),
     };
     let op_config_2 = OmniPaxosConfig {
         server_config: server_config_2,
@@ -43,23 +39,23 @@ fn main() {
     };
     let  storage:MemoryStorage<OmniLogEntry> = MemoryStorage::default();
     let omni_paxos = op_config
-        .build(storage)
+        .build(storage.clone())
         .expect("failed to build OmniPaxos");
     let omni_paxos_2 = op_config_2
-    .build(storage.clone())
+    .build(storage)
     .expect("failed to build OmniPaxos");
     let mut node: Node = Node::new(1, OmniPaxosDurability::new(omni_paxos));
     let mut node_2: Node = Node::new(2, OmniPaxosDurability::new(omni_paxos_2));
     
-    let  (sending_channels, receiver_channels) = initialise_channels();
+    let  (sending_channels, mut receiver_channels) = initialise_channels();
     
 
-    let  node_runner = NodeRunner {
+    let mut  node_runner = NodeRunner {
         node: Arc::new(Mutex::new(node)),
         incoming: receiver_channels.remove(&1).unwrap(),
         outgoing: sending_channels.clone(),
     };
-    let  node_runner_2 = NodeRunner {
+    let mut  node_runner_2 = NodeRunner {
         node: Arc::new(Mutex::new(node_2)),
         incoming: receiver_channels.remove(&2).unwrap(),
         outgoing: sending_channels.clone(),
@@ -91,7 +87,7 @@ fn initialise_channels() -> (
     let mut receiver_channels = HashMap::new();
 
     for pid in SERVERS {
-        let (sender, receiver) = mpsc::channel(BUFFER_SIZE);
+        let (sender, receiver) = mpsc::channel(4);
         sender_channels.insert(pid, sender);
         receiver_channels.insert(pid, receiver);
     }
